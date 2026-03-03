f611d6b8741ee86eaf0d57b50923b42132c991f5
George Spanogiannakis <139562803+Spg33@users.noreply.github.com>
2026-03-03 11:31:00 +0100

    Login check script for user authentication
    Applying python automation in context of Google Cybersecurity Professional Certificate.
    
    Implement login check functionality to count failed attempts.

---
+++ login_check_script.py
@@ -0,0 +1,19 @@
+# open, read and split a text file
+
+with open("filename.txt", "r") as file:  # Replace "filename.txt" with the file intended to use
+    file_text = file.read()
+usernames = file_text.split()
+
+# Function that counts a user's failed login attempts
+
+def login_check(login_list, current_user):
+    counter = 0
+    for i in login_list:
+        if i == current_user:
+            counter = counter + 1
+    if counter >= 3:
+        return "You tried to login 3 or more times, therefore your account has been locked!"
+    else:
+        return "You can login!"
+
+login_check(usernames, "user")  # call the function & replace the "user" with the username
