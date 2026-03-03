529924750a8de7f96db3f1d1e352e6e72ce0945f
George Spanogiannakis <139562803+Spg33@users.noreply.github.com>
2026-03-03 11:40:27 +0100

    Update C_to_F.py

--- C_to_F.py
+++ C_to_F.py
@@ -1,15 +1,21 @@
+# add 2 lists, one for each measurement option Celsius and Fahrenheit
+
 celsius_temperatures = []
 fahrenheit_temperatures = []
 
-# get values from user
-for i in range(3):
+# get 3 values from user input
+
+for i in range(3):   # modify this for more inputs
     value = float(input("Enter Celsius: "))
     celsius_temperatures.append(value)
 
-# convert values
-for celsius in celsius_temperatures:
+# convert values from Celsius to Fahrenheit
+
+for celsius in celsius_temperatures:   # you can reverse according to your needs
     fahrenheit = (celsius * 9/5) + 32
     fahrenheit_temperatures.append(fahrenheit)
+
+# print the results for both measurements
 
 print("Celsius Temperatures:", celsius_temperatures)
 print("Fahrenheit Temperatures:", fahrenheit_temperatures)
